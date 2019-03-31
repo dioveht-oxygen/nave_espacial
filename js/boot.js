10eb19436b06d7853e5869dceb9e04209f16c541
Dioveht <kiritosao1526@gmail.com>
2019-04-01 01:26:47 +0700

    new update enemys and danger

--- js/boot.js
+++ js/boot.js
@@ -1,7 +1,9 @@
 var canvas = document.getElementById("canvas");
 var draw = canvas.getContext("2d");
 var action = 0;
-var reload = 0;
+var score = 0;
+var attack = 0.2;
+var lost = false;
 //cấu hình thân thuyền
 body = [
     {x: 150, y: 440},
@@ -10,12 +12,12 @@ body = [
     {x: 160, y: 450}
 ];
 
+
 bullets = [
     {x: body[0].x, y: body[1].y - 20},
     {x: body[0].x, y: body[1].y - 160},
     {x: body[0].x, y: body[1].y - 300},
 ];
-
 
 var army_1 = [
     {life: 3, start: {x: 10, y: 20}},
@@ -131,7 +133,7 @@ spaceship = {
     //nạp đạn
     resetBullet: function () {
         for (let i = 0; i < this.bullet.bullets.length; i++) {
-            if (this.bullet.bullets[i].y < 0 || reload == 35) {
+            if (this.bullet.bullets[i].y < 0) {
                 bullets.push({
                     x: body[0].x, y: body[1].y - 140 * i
                 });
@@ -139,9 +141,13 @@ spaceship = {
             }
         }
         console.log("func resetBullet success");
+    },
+    score: function () {
+        document.getElementById("score").innerHTML = score;
     }
 };
 
+//cấu hình kẻ địch
 var enemys = {
     army_1: army_1,
     army_2: army_2,
@@ -160,51 +166,50 @@ army = {
             if (this.enemys.army_1[j].life == 3) {
                 draw.fillStyle = this.color.lv3;
             }
+            //else if (this.enemys.army_2[j].life < 0) draw.fillStyle = "white";
             else if (this.enemys.army_1[j].life == 2) {
                 draw.fillStyle = this.color.lv2;
             }
-            else if(this.enemys.army_1[j].life == 1) {
-                draw.fillStyle = this.color.lv1;
-            }
-            else draw.fillStyle = "white";
-            draw.fillRect(this.enemys.army_1[j].start.x,this.enemys.army_1[j].start.y, 10, 10);
+            else draw.fillStyle = this.color.lv1;
+            draw.fillRect(this.enemys.army_1[j].start.x, this.enemys.army_1[j].start.y, 10, 10);
         }
         console.log("draw Army_1 success");
         for (let j = 0; j < this.enemys.army_2.length; j++) {
             if (this.enemys.army_2[j].life == 3) {
                 draw.fillStyle = this.color.lv3;
             }
+            //else if (this.enemys.army_2[j].life < 0) draw.fillStyle = "white";
             else if (this.enemys.army_2[j].life == 2) {
                 draw.fillStyle = this.color.lv2;
             }
-            else if(this.enemys.army_1[j].life == 1) {
+            else {
                 draw.fillStyle = this.color.lv1;
             }
-            else draw.fillStyle = "white";
-            draw.fillRect(this.enemys.army_2[j].start.x,this.enemys.army_2[j].start.y, 10, 10);
+            ;
+            draw.fillRect(this.enemys.army_2[j].start.x, this.enemys.army_2[j].start.y, 10, 10);
         }
         console.log("draw Army_2 success");
         for (let j = 0; j < this.enemys.army_3.length; j++) {
             if (this.enemys.army_3[j].life == 3) {
                 draw.fillStyle = this.color.lv3;
             }
+            //else if (this.enemys.army_3[j].life < 0) draw.fillStyle = "white";
             else if (this.enemys.army_3[j].life == 2) {
                 draw.fillStyle = this.color.lv2;
             }
-            else if(this.enemys.army_1[j].life == 1) {
+            else {
                 draw.fillStyle = this.color.lv1;
             }
-            else draw.fillStyle = "white";
-            draw.fillRect(this.enemys.army_3[j].start.x,this.enemys.army_3[j].start.y, 10, 10);
+            draw.fillRect(this.enemys.army_3[j].start.x, this.enemys.army_3[j].start.y, 10, 10);
         }
         console.log("draw Army_3 success");
         console.log("func drawEnemy success");
     },
     attack: function () {
         for (let j = 0; j < enemys.army_1.length; j++) {
-            for (let i=0;i<bullets.length;i++){
-                if(enemys.army_1[j].life < 0) enemys.army_1.slice(j , 1);
-                else if(bullets[i].x == enemys.army_1[j].start.x && bullets[i].y == enemys.army_1[j].start.y  ){
+            for (let i = 0; i < bullets.length; i++) {
+                if (enemys.army_1[j].life == 0){ army_1.splice(j,1); score += 3;}
+                else if (bullets[i].x == enemys.army_1[j].start.x && bullets[i].y == enemys.army_1[j].start.y) {
                     bullets.push({
                         x: body[0].x, y: body[1].y - 140 * i
                     });
@@ -213,11 +218,11 @@ army = {
                 }
             }
         }
-        console.log("draw attack army_1 success");
+        console.log("draw attack army_1 success : " + army_1.length);
         for (let j = 0; j < enemys.army_2.length; j++) {
-            for (let i=0;i<bullets.length;i++){
-                if(enemys.army_2[j].life < 0) enemys.army_2.slice(j , 1);
-                else if(bullets[i].x == enemys.army_2[j].start.x && bullets[i].y == enemys.army_2[j].start.y  ){
+            for (let i = 0; i < bullets.length; i++) {
+                if (enemys.army_2[j].life < 0){ army_2.splice(j, 1);score += 3;}
+                else if (bullets[i].x == enemys.army_2[j].start.x && bullets[i].y == enemys.army_2[j].start.y) {
                     bullets.push({
                         x: body[0].x, y: body[1].y - 140 * i
                     });
@@ -228,9 +233,9 @@ army = {
         }
         console.log("draw attack army_2 success");
         for (let j = 0; j < enemys.army_3.length; j++) {
-            for (let i=0;i<bullets.length;i++){
-                if(enemys.army_3[j].life < 0) enemys.army_3.slice(j , 1);
-                else if(bullets[i].x == enemys.army_3[j].start.x && bullets[i].y == enemys.army_3[j].start.y  ){
+            for (let i = 0; i < bullets.length; i++) {
+                if (enemys.army_3[j].life < 0){ army_3.splice(j, 1) ; score += 3;}
+                else if (bullets[i].x == enemys.army_3[j].start.x && bullets[i].y == enemys.army_3[j].start.y) {
                     bullets.push({
                         x: body[0].x, y: body[1].y - 140 * i
                     });
@@ -242,14 +247,6 @@ army = {
         console.log("draw attack army_3 success");
     }
 };
-
-//vẽ game
-function drawAll() {
-    spaceship.clearAll();
-    army.drawArmy();
-    spaceship.drawSpaceship();
-    spaceship.drawBullet(0);
-}
 
 //điều khiển
 function control(e) {
@@ -286,12 +283,127 @@ function control(e) {
 }
 document.addEventListener("keydown", control);
 
-setInterval(function () {
+var dangers = [
+    {
+        x: Math.floor(Math.random() * 270) + 30,
+        y: Math.floor(Math.random() * 400) + 100,
+        //with
+        w: Math.floor(Math.random() * 30) + 10,
+        //speed
+        h: Math.floor(Math.random() * 30) + 10
+    },
+    {
+        x: Math.floor(Math.random() * 270) + 30,
+        y: Math.floor(Math.random() * 400) + 100,
+        //with
+        w: Math.floor(Math.random() * 30) + 10,
+        //speed
+        h: Math.floor(Math.random() * 30) + 10
+    },
+    {
+        x: Math.floor(Math.random() * 270) + 30,
+        y: Math.floor(Math.random() * 400) + 100,
+        //with
+        w: Math.floor(Math.random() * 30) + 10,
+        //speed
+        h: Math.floor(Math.random() * 30) + 10
+    },
+    {
+        x: Math.floor(Math.random() * 270) + 30,
+        y: Math.floor(Math.random() * 400) + 100,
+        //with
+        w: Math.floor(Math.random() * 30) + 10,
+        //speed
+        h: Math.floor(Math.random() * 30) + 10
+    }
+];
+danger = {
+    dangers: dangers,
+
+    newDanger: function () {
+        dangers.splice(0);
+      for (let i = 0 ; i < 6 ; i++){
+        dangers.push({
+            x: Math.floor(Math.random() * 270) + 30,
+            y: Math.floor(Math.random() * 400) + 20,
+            //with
+            w: Math.floor(Math.random() * 20) + 10,
+            //speed
+            h: Math.floor(Math.random() * 20) + 10,
+            s: Math.floor(Math.random() * 5) + 1
+        });
+      }
+    },
+    drawDanger: function () {
+        if(action == 50) danger.newDanger();
+        for(let i=0 ; i < danger.dangers.length ; i++){
+            draw.fillStyle = "black";
+            draw.fillRect(danger.dangers[i].x,danger.dangers[i].y, danger.dangers[i].w,danger.dangers[i].h);
+        }
+        console.log("func danger.drawDanger success");
+    },
+    attack: function () {
+        for(let i = 0 ; i < dangers.length ; i++){
+            if(danger.dangers[i].w < danger.dangers[i].h){
+                dangers[i].y += danger.dangers[i].s;
+                danger.dangers[i].s += attack;
+            }else {
+                dangers[i].x += danger.dangers[i].s;
+                danger.dangers[i].s += attack;
+            }
+        }
+        console.log("func danger.attack success");
+    },
+    lost: function () {
+        for(let i = 0 ; i < dangers.length ; i++){
+            for(let j = 0 ; j < spaceship.body.length ; j++){
+                if(danger.dangers[i].x > spaceship.body[j].x || (danger.dangers[i].y + danger.dangers[i].h) < spaceship.body[j].y ){
+
+                }
+                else if ((danger.dangers[i].x + danger.dangers[i].w) < spaceship.body[j].x){
+                    if ((danger.dangers[i].y + danger.dangers[i].h) > spaceship.body[j].y){
+                         lost = true;
+                     }
+                }
+            }
+        }
+    }
+};
+
+//vẽ game
+function drawAll() {
+    spaceship.clearAll();
+    army.drawArmy();
+    spaceship.drawSpaceship();
+    spaceship.drawBullet();
+    danger.drawDanger();
+}
+
+// game
+var programs =  setInterval(function () {
+    spaceship.score();
+    checkWin();
     army.attack();
+    danger.attack();
     spaceship.resetBullet();
     spaceship.moveBullet(action);
-    action = 0;
+    if (action == 50) action=0;
+    action++;
     drawAll();
-    // if (reload == 35) reload;
-    // reload++;
-}, 30);
+    danger.lost();
+}, 20);
+
+// khởi động
+function checkWin() {
+    if(army_1 == 0){
+        let test = confirm("Bạn đã thắng bạn có muốn chơi lại không ??");
+        if (test == true){clearInterval(programs); location.reload();}
+        else{clearInterval(programs); alert("cảm ơn vì bạn đã chơi thử !"); }
+    }
+    else if(lost == true){
+        let test = confirm("Bạn đã thua bạn có muốn chơi lại không ??");
+        if (test == true){clearInterval(programs); location.reload();}
+        else{clearInterval(programs); alert("cảm ơn vì bạn đã chơi thử !"); }
+    }
+}
+
